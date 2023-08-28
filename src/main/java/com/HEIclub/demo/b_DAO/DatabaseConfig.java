dae2241f6a248cca4a7dbbaebf43a464119d4956
Nante1805 <hei.nantenaina.2@gmail.com>
2023-08-28 18:24:04 +0300

    feat : "initialise Connection"

---
+++ src/main/java/com/HEIclub/demo/b_DAO/DatabaseConfig.java
@@ -0,0 +1,27 @@
+package com.HEIclub.demo.b_DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
+public class DatabaseConfig {
+    @Value("${DATABASE_URL}")
+    private String dbUrl;
+
+    @Value("${PSQL_USER_NAME}")
+    private String dbUsername;
+
+    @Value("${PSQL_PWD}")
+    private String dbPassword;
+
+    @Bean
+    public Connection getDatabaseConnection() throws SQLException {
+        return DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+    }
+
+}
